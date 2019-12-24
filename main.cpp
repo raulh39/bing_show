719a5517ce778f5f3452f312d1672898d50bbb5a
Raul Huertas <raulh39@gmail.com>
2019-12-24 15:28:46 +0100

    Parse URIs

--- main.cpp
+++ main.cpp
@@ -19,8 +19,14 @@ int main()
         if(response.status_code() != 200) return 1;
 
         auto json = response.extract_json().get(); //Note: get() make this block
-        auto url_image_0 = json["images"].at(0)["url"];
-        fmt::print("URL image 0: {}\n", url_image_0.as_string());
+
+        auto url_image_0 = json["images"].at(0)["url"].as_string(); //This will have something similar to: /th?id=OHR.ReindeerNorway_ES-ES0018263159_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp
+        fmt::print("URL image 0: {}\n", url_image_0);
+        
+        //We need to extract the name of the file name in the previous URL:
+        web::uri image_uri{url_image_0};
+        auto filename = web::uri::split_query(image_uri.query())["id"];
+        fmt::print("Image filename: {}\n", filename);
         
     } catch (web::http::http_exception &e) {
         fmt::print("Terminating by http_exception: {}\n", e.what());
